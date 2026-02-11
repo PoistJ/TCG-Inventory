0de67db7820fa1c4c83de6f085b63415b714c4a0
PoistJ <157406020+PoistJ@users.noreply.github.com>
2026-02-10 23:21:05 -0500

    Implemented route and function to view card info when clicked in inventory.

--- db/queries.js
+++ db/queries.js
@@ -12,7 +12,15 @@ async function insertMTG(card_name, cost, rarity, quantity) {
   );
 }
 
+async function getCard(id) {
+  const { rows } = await pool.query("SELECT * FROM mtg WHERE card_id = ($1)", [
+    id,
+  ]);
+  return rows[0];
+}
+
 module.exports = {
   getMTG,
   insertMTG,
+  getCard,
 };
